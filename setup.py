96c715b271cf7f750b1435ea59fe5b88159c9fbf
Jason L Weirather <jason-weirather@users.noreply.github.com>
2019-03-13 11:26:59 -0400

    as pythologist has changed restrict this software to the earlier release

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(path.join(this_folder,'README.md'),encoding='utf-8') as inf:
 
 setup(
   name='informverify',
-  version='0.1.4',
+  version='0.1.5',
   description='inForm PerkinElmer file assumption verification',
   long_description=long_description,
   url='https://github.com/jason-weirather/inform-verify',
@@ -23,7 +23,7 @@ setup(
   ],
   keywords='bioinformatics',
   packages=['informverify'],
-  install_requires = ['pythologist>=0.1.10','Pillow>=2.2.1'],
+  install_requires = ['pythologist==0.1.10','Pillow>=2.2.1'],
   entry_points = {
     'console_scripts':['informverify=informverify:entry_point']
   }
